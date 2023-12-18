cc3011100487166fc6e9d49cd2d0558b9d128979
Koson2006 <137600083+Koson2006@users.noreply.github.com>
2023-12-18 16:50:16 +0700

    Add TAB

--- App1/App1/MenuPage.xaml.cs
+++ App1/App1/MenuPage.xaml.cs
@@ -15,6 +15,51 @@ namespace App1
         public MenuPage()
         {
             InitializeComponent();
+            YouBT.Clicked += YouBT_Clicked;
+            GridBT.Clicked += GridBT_Clicked;
+            StackBT.Clicked += StackBT_Clicked;
+            TabBT.Clicked += TabBT_Clicked;
+            ProjBT.Clicked += ProjBT_Clicked;
+        }
+
+        private void ProjBT_Clicked(object sender, EventArgs e)
+        {
+            var mp = Parent as FlyoutPage;
+            mp.Detail = new NavigationPage(new Proj());
+            mp.IsPresented = false;
+        }
+
+        private void TabBT_Clicked(object sender, EventArgs e)
+        {
+            var TP = new TabbedPage();
+            TP.Children.Add(new TabPage1());
+            TP.Children.Add(new TabPage2());
+            TP.Children.Add(new TabPage3());
+
+            var mp = Parent as FlyoutPage;
+            mp.Detail = new NavigationPage(TP);
+            mp.IsPresented = false;
+        }
+
+        private void StackBT_Clicked(object sender, EventArgs e)
+        {
+            var mp = Parent as FlyoutPage;
+            mp.Detail = new NavigationPage(new YoutubePage());
+            mp.IsPresented = false;
+        }
+
+        private void GridBT_Clicked(object sender, EventArgs e)
+        {
+            var mp = Parent as FlyoutPage;
+            mp.Detail = new NavigationPage(new NavPage1());
+            mp.IsPresented = false;
+        }
+
+        private void YouBT_Clicked(object sender, EventArgs e)
+        {
+            var mp = Parent as FlyoutPage;
+            mp.Detail = new NavigationPage(new YoutubePage());
+            mp.IsPresented = false;
         }
     }
 }
